5a094bcdb79c75085bca6dee38e59d69aebe7f34
ebinee <skyim0907@naver.com>
2025-03-30 16:08:19 +0900

    ✨ Feat: Add components

--- semothon/src/components/Header.jsx
+++ semothon/src/components/Header.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "../styles/Header.css";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AiFillProfile } from "react-icons/ai";
+import { Popover } from "./Popover";
 
-const Header = () => {
-  return <header>헤더입니다</header>;
+const Header = ({ isLogin }) => {
+  const navigate = useNavigate();
+
+  // 로고 클릭 시 메인페이지로 이동
+  const goToMain = () => {
+    navigate("/");
+  };
+
+  return (
+    <header>
+      <div className="header-logo" onClick={goToMain}>
+        로고
+      </div>
+      {!isLogin ? (
+        <div className="guest-menu">
+          <Link to="/login">로그인</Link>
+          <Link to="/logout">로그아웃</Link>
+        </div>
+      ) : (
+        <div className="user-menu">
+          <Popover trigger={<AiFillProfile size={36} />}>
+            <div className="popover-item">마이페이지</div>
+            <hr className="popover-divider" />
+            <div className="popover-item">로그아웃</div>
+          </Popover>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Header;
--- semothon/src/pages/Main.jsx
+++ semothon/src/pages/Main.jsx
@@ -1,18 +1,26 @@
-import React from "react";
-import Sidebar from "../components/Sidebar";
+import React, { useState } from "react";
+import SideBar from "../components/SideBar";
 import SearchBar from "../components/SearchBar";
 import "../styles/Main.css";
 
 const Main = () => {
+  const [value, setValue] = useState(0);
   return (
     <div className="main">
       <div className="layout-sidebar">
-        <Sidebar />
+        <SideBar />
       </div>
       <div className="layout-main">
-        <div className="main-logo">로고입니다.</div>
+        <div className="main-logo">KHUUU</div>
         <div className="search-input">
           <SearchBar />
+        </div>
+        <div>
+          <p>
+            현재 카운터 값은 <b>{value}</b> 입니다.
+          </p>
+          <button onClick={() => setValue(value + 1)}>+1</button>
+          <button onClick={() => setValue(value - 1)}>-1</button>
         </div>
       </div>
     </div>
